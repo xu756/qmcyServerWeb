30ec2206028f05adcd7031682af6c29f427b68a1
xu756 <756334744@qq.com>
2023-11-15 16:36:46 +0800

    修改docker

--- common/xerr/errors.go
+++ common/xerr/errors.go
@@ -10,43 +10,43 @@ import (
 */
 
 type CodeError struct {
-	errCode uint32
-	errMsg  string
+	ErrCode uint32
+	ErrMsg  string
 }
 
 // GetErrCode 返回给前端的错误码
 func (e *CodeError) GetErrCode() uint32 {
-	return e.errCode
+	return e.ErrCode
 }
 
 // GetErrMsg 返回给前端显示端错误信息
 func (e *CodeError) GetErrMsg() string {
-	return e.errMsg
+	return e.ErrMsg
 }
 
 func (e *CodeError) Error() string {
-	return fmt.Sprintf("ErrCode:%d，ErrMsg:%s", e.errCode, e.errMsg)
+	return fmt.Sprintf("ErrCode:%d，ErrMsg:%s", e.ErrCode, e.ErrMsg)
 }
 
 // NewSystemError 系统错误
 func NewSystemError(err interface{}) *CodeError {
 	logx.Error("【系统错误】%v", err)
-	return &CodeError{errCode: NOTIFICATION, errMsg: "系统错误,请联系管理员"}
+	return &CodeError{ErrCode: NOTIFICATION, ErrMsg: "系统错误,请联系管理员"}
 }
 
 // NewDbErr 数据库错误
 func NewDbErr(msg string, err interface{}) *CodeError {
 	logx.Error("【数据库错误  %s】---%v", msg, err)
-	return &CodeError{errCode: ERRORMESSAGE, errMsg: "系统错误,请联系管理员"}
+	return &CodeError{ErrCode: ERRORMESSAGE, ErrMsg: "系统错误,请联系管理员"}
 }
 
 // NewMsgError 通用错误（返回给用户的）
 func NewMsgError(msg string) *CodeError {
-	return &CodeError{errCode: WarnMessage, errMsg: msg}
+	return &CodeError{ErrCode: WarnMessage, ErrMsg: msg}
 }
 
 // LogOut  重新登录
 func LogOut() *CodeError {
-	return &CodeError{errCode: REDIRECT, errMsg: "身份验证失败，请重新登录"}
+	return &CodeError{ErrCode: REDIRECT, ErrMsg: "身份验证失败，请重新登录"}
 
 }
